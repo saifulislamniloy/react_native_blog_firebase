7d757dbb42c36eb50b2425b063d50d90e730334b
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-12-14 11:28:17 +0600

    Stack Navigator Used

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+
+const AuthStack = createStackNavigator();
+
+const AuthStackScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
+      <AuthStack.Screen name="SignInScreen" component={SignInScreen} options={{ title: "Sign In" }} />
+      <AuthStack.Screen name="SignUpScreen" component={SignUpScreen} options={{ title: "Sign Up" }} />
+    </AuthStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthStackScreen />
+    </NavigationContainer>
+  );
+}
